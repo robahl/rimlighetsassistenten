be685d82bffc091005b5655d0ffbc171287ec49c
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-10-14 16:33:48 +0200

    Initial commit

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <p>Calculator</p>
+    );
+  }
+}
+
+export default Calculator;
---
+++ src/Result.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Result(props) {
+  return (
+    <p>This is {props.result}</p>
+  );
+}
+
+export default Result;
